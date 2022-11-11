9e614ac1b29ab5397d797de3345973856abed010
MrRojodgaf <112688440+MrRojodgaf@users.noreply.github.com>
2022-11-11 09:44:52 +0100

    moved

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const posts = [
         username: "gus1819",
         location: "Ornans, France",
         avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
+        post: "post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
     },
@@ -21,8 +21,8 @@ const posts = [
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
+        avatar: "avatar-ducreux.jpg",
+        post: "post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
     }
